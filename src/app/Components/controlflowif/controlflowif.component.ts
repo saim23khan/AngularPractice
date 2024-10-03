ac37896f6a729983687768657bc79b43f22a62ca
Saim Khan <sayemkhan2345@gmail.com>
2024-10-04 01:20:42 +0500

    Working for for & switch

--- src/app/Components/controlflowif/controlflowif.component.ts
+++ src/app/Components/controlflowif/controlflowif.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-controlflowif',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,RouterOutlet,RouterLink],
   templateUrl: './controlflowif.component.html',
   styleUrl: './controlflowif.component.css'
 })
@@ -13,7 +14,20 @@ export class ControlflowifComponent {
   btnchk : boolean = false ;
   nametxtbox : string ='' ;
   chkgendercase : string ='' ;
+  batchtxtget : string = '';
   
+ studentlistfor: string [] = ['Saim Khan','Waqar Ali','Asad Ali','Dilshad Ahmer'] ;
+
+ detaillistofstdnt : any[] =[
+    {Id:11, Named:'Saim Khan', shift:'Morning',     batch:'241224F', Active: true} ,
+    {Id:112, Named:'Faraz Shahab', shift:'Evening', batch:'241226G', Active: true} ,
+    {Id:113, Named:'Asad Ali', shift:'Afternoon',   batch:'241235G', Active: false} ,
+    {Id:114, Named:'Waqar Khan', shift:'Afternoon', batch:'241224F', Active: true} ,
+    {Id:115, Named:'Sheikh Umer', shift:'morning',  batch:'241226A', Active: true} ,
+    {Id:116, Named:'Taha Siddiqui', shift:'Evening',batch:'241224F', Active: false} ,
+    {Id:117, Named:'Faiq Khan', shift:'morning',    batch:'241226A', Active: true} 
+ ]
+
   btnclickname(){
     this.btnchk = !this.btnchk ;
   }
