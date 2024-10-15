ceb6df86334eeeac9d59586126b384dbd2a22287
Saim Khan <sayemkhan2345@gmail.com>
2024-10-16 01:22:11 +0500

    working for formpage

--- src/app/Components/Forms/TemplatefoamPage/user-foam-page/user-foam-page.component.ts
+++ src/app/Components/Forms/TemplatefoamPage/user-foam-page/user-foam-page.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-foam-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,JsonPipe],
   templateUrl: './user-foam-page.component.html',
   styleUrl: './user-foam-page.component.css'
 })
 export class UserFoamPageComponent {
+
+Foamlistvalue  : any = {
+  firstname : '' ,
+  lastname : '' ,
+  fathername: '',
+  email : '' ,
+  city : '' ,
+  Statevalue:'',
+  isaccept : false 
+}
+formvalue : any ;
+
+btnsubmit() {
+  
+  this.formvalue = this.Foamlistvalue
+}
 
 }
